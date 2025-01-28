ba5b3261da2db91a492b1f1fe0c110bd850196ca
chente09 <chentenengerc@gmail.com>
2025-01-27 23:11:07 -0500

    nav-bar hover

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,9 +10,14 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzIcons(icons), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzIcons(icons), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({ projectId: "aplasta-insectos", appId: "1:10832189466:web:b1d017bef5075ea2b8d9a3", databaseURL: "https://aplasta-insectos-default-rtdb.firebaseio.com", storageBucket: "aplasta-insectos.appspot.com", apiKey: "AIzaSyCj5G0mMJ1f3JZDTwTftuUMQs6G0OY_A7M", authDomain: "aplasta-insectos.firebaseapp.com", messagingSenderId: "10832189466" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
 };
